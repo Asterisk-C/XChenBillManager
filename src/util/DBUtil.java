84d915757ef2d037b7fa5404848df60fbb8a40e0
Asterisk-C <hsinhaochen334@gmail.com>
2022-08-19 16:28:24 +0100

    Entities added.

---
+++ src/util/DBUtil.java
@@ -0,0 +1,12 @@
+package util;
+
+public class DBUtil 
+{
+	static String ip = "127.0.0.1";
+	static int port = 3306;
+	static String database = " ";
+    static String encoding = "UTF-8";
+    static String loginName = " ";
+    static String password = " ";
+
+}
