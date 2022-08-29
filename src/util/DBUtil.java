c3f5c0a243ce22fd198889e0bed9515e69c0db99
Asterisk-C <hsinhaochen334@gmail.com>
2022-08-29 16:39:52 +0100

    2 Utils added.

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBUtil 
 {
 	static String ip = "127.0.0.1";
@@ -8,5 +12,18 @@ public class DBUtil
     static String encoding = "UTF-8";
     static String loginName = " ";
     static String password = " ";
+    static {
+    	try {
+    		Class.forName("com.mysql.jdbc.Driver");
+    	}catch(ClassNotFoundException e) {
+    		e.printStackTrace();
+    	}
+    }
+    
+    public static Connection getConnection() throws SQLException
+    {
+    	String url = String.format("jdbc:mysql://%s:%d/%s?characterEncoding=%s", ip, port, database, encoding);
+    	return DriverManager.getConnection(url, loginName, password);
+    }
 
 }
